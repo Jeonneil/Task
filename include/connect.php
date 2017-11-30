1f6f36ea50419752ea3712ab13db8e8c0fd7e690
jeonneil <jeonblanco73@gmail.com>
2017-11-30 11:10:32 +0800

    patch

--- include/connect.php
+++ include/connect.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
-$username = "id3744092_root";
-$password = "Admin123!";
-$dbname = "id3744092_vercons_db";
+$username = "root";
+$password = "";
+$dbname = "vercons_db";
 $conn =  new mysqli($servername, $username, $password, $dbname);
 ?>
